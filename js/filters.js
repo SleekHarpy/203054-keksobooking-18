4a129b2c67c825bd568db71fa258552fd92bbdd2
Влад <rtlistate@gmail.com>
2019-11-04 19:13:12 +0300

    Сделан фильтр по цене

--- js/filters.js
+++ js/filters.js
@@ -2,15 +2,49 @@
 
 (function () {
   var QUANTITY_PINS = 5;
+  var MIN_PRICE = 10000;
+  var MAX_PRICE = 50000;
 
   var mapFilters = document.querySelector('.map__filters');
   var housingType = mapFilters.querySelector('#housing-type');
+  var housingRooms = mapFilters.querySelector('#housing-rooms');
+  var housingGuests = mapFilters.querySelector('#housing-guests');
+  var housingPrice = mapFilters.querySelector('#housing-price');
+
 
   window.filters = {
     getData: function (data) {
+
+      var isMatchPrice = function (elem) {
+        var result = false;
+
+        switch (housingPrice.value) {
+          case 'any':
+            result = true;
+            break;
+          case 'low':
+            result = elem.offer.price < MIN_PRICE;
+            break;
+          case 'middle':
+            result = elem.offer.price >= MIN_PRICE && elem.offer.price <= MAX_PRICE;
+            break;
+          case 'high':
+            result = elem.offer.price > MAX_PRICE;
+            break;
+        }
+
+        return result;
+      };
+
       var changeDataPins = function () {
         var dataPins = data.filter(function (pins) {
           return pins.offer.type === housingType.value || housingType.value === 'any';
+        }).filter(function (rooms) {
+          return rooms.offer.rooms === Number(housingRooms.value) || housingRooms.value === 'any';
+        }).filter(function (guests) {
+          return guests.offer.guests === Number(housingGuests.value) || housingGuests.value === 'any';
+        }).filter(function (price) {
+          return isMatchPrice(price);
         }).slice(0, QUANTITY_PINS);
 
         return dataPins;
@@ -22,7 +56,7 @@
         window.map.renderPins(changeDataPins());
       };
 
-      housingType.addEventListener('change', onFiltersChange);
+      mapFilters.addEventListener('change', onFiltersChange);
 
       return changeDataPins();
     }
