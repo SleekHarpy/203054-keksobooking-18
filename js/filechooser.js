0aa7710a28a1e64de9d0a321d590e31193bdb142
Влад <rtlistate@gmail.com>
2019-11-09 00:25:36 +0300

    Сделано добавление нескольких фотографий жилья

--- js/filechooser.js
+++ js/filechooser.js
@@ -9,20 +9,36 @@
   var fileChooserAvatar = document.querySelector('#avatar');
   var previewAvatar = document.querySelector('.ad-form-header__preview img');
   var fileChooserImage = document.querySelector('#images');
-  var previewImageContainer = document.querySelector('.ad-form__photo');
-  var previewImage;
+  var previewImageContainer = document.querySelector('.ad-form__photo-container');
+  var previewImageBox = document.querySelector('.ad-form__photo');
+  var previewImage = document.querySelector('.ad-form__photo img');
 
-  var renderImgElement = function (content) {
-    previewImage = document.createElement('img');
-    previewImage.src = content.result;
-    previewImage.width = IMAGE_WIDTH;
-    previewImage.height = IMAGE_HEIGTH;
-    previewImage.style.margin = IMAGE_MARGIN;
-    previewImageContainer.appendChild(previewImage);
+  var renderElements = function (content) {
+    var createImgElement = function () {
+      previewImage = document.createElement('img');
+      previewImage.src = content.result;
+      previewImage.width = IMAGE_WIDTH;
+      previewImage.height = IMAGE_HEIGTH;
+      previewImage.style.margin = IMAGE_MARGIN;
+
+      return previewImage;
+    };
+
+    var createImgContainer = function () {
+      var previewDiv = document.createElement('div');
+      previewImageContainer.appendChild(previewDiv);
+      previewDiv.classList.add('ad-form__photo');
+      previewDiv.appendChild(createImgElement());
+    };
+    if (!previewImage) {
+      previewImageBox.appendChild(createImgElement());
+    } else {
+      createImgContainer();
+    }
   };
 
 
-  var onChooseFile = function (choose, preview, isExistImg) {
+  var onChooseFile = function (choose, preview) {
     var file = choose.files[0];
     var fileName = file.name.toLowerCase();
 
@@ -34,10 +50,10 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        if (!isExistImg) {
-          renderImgElement(reader);
-        } else {
+        if (preview !== true & preview !== previewImage) {
           preview.src = reader.result;
+        } else {
+          renderElements(reader);
         }
       });
 
@@ -46,16 +62,22 @@
   };
 
   fileChooserAvatar.addEventListener('change', function () {
-    onChooseFile(fileChooserAvatar, previewAvatar, true);
+    onChooseFile(fileChooserAvatar, previewAvatar);
   });
 
   fileChooserImage.addEventListener('change', function () {
-    onChooseFile(fileChooserImage, previewImage, false);
+    onChooseFile(fileChooserImage, previewImage);
   });
 
   window.cleanChooser = function () {
+    var dataPreviewImageBoxes = Array.from(document.querySelectorAll('.ad-form__photo'));
+
     previewAvatar.src = 'img/muffin-grey.svg';
+
     if (previewImage) {
+      for (var i = 1; i < dataPreviewImageBoxes.length; i++) {
+        dataPreviewImageBoxes[i].remove();
+      }
       previewImage.remove();
     }
   };
